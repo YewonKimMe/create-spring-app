38e452ea47f9350c0de94561afc8fc0fe589c580
kim-yewon <kimyewon030428@gmail.com>
2025-07-04 11:43:11 +0900

    feat: 시큐리티 관련 사용자 역할 enum Role 추가

---
+++ src/main/java/com/github/YewonKimMe/create_spring_app/security/enums/Role.java
@@ -0,0 +1,16 @@
+package com.github.YewonKimMe.create_spring_app.security.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum Role {
+
+    PREFIX("ROLE_"),
+    MEMBER("MEMBER"),   // 일반 회원
+    ADMIN("ADMIN"),     // 관리자
+    GUEST("GUEST");     // 비회원/게스트
+
+    private final String role;
+}
