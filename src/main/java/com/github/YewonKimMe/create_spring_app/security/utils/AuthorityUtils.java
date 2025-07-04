0296418213ca30f799a313756dc9fec07922a44b
kim-yewon <kimyewon030428@gmail.com>
2025-07-04 14:47:39 +0900

    feat: AuthorityUtils Stream 으로 변경

--- src/main/java/com/github/YewonKimMe/create_spring_app/security/utils/AuthorityUtils.java
+++ src/main/java/com/github/YewonKimMe/create_spring_app/security/utils/AuthorityUtils.java
@@ -3,15 +3,12 @@ package com.github.YewonKimMe.create_spring_app.security.utils;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 public class AuthorityUtils {
     public static String populateAuthorities(Collection<? extends GrantedAuthority> collections) {
-        Set<String> authoritiesSet = new HashSet<>();
-        for (GrantedAuthority authority : collections) {
-            authoritiesSet.add(authority.getAuthority());
-        }
-        return String.join(",", authoritiesSet);
+        return collections.stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
     }
 }
