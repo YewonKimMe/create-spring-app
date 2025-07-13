781875fcb4b9f985e96ee58b3271a92719d6f28a
kim-yewon <kimyewon030428@gmail.com>
2025-07-13 22:13:01 +0900

    chore: return 제거

--- src/main/java/com/github/YewonKimMe/create_spring_app/security/filter/JwtValidatorFilter.java
+++ src/main/java/com/github/YewonKimMe/create_spring_app/security/filter/JwtValidatorFilter.java
@@ -95,7 +95,6 @@ public class JwtValidatorFilter extends OncePerRequestFilter {
             }
             default: {
                 cleanContextAndContinue(request, response, filterChain);
-                return;
             }
         }
     }
