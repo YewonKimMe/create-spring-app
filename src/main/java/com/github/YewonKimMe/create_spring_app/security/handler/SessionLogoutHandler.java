24cd2b3174eb14a9d36b85c982f8023e36a1134d
kim-yewon <kimyewon030428@gmail.com>
2025-11-05 14:56:16 +0900

    feat: session logout handler

--- src/main/java/com/github/YewonKimMe/create_spring_app/security/handler/SessionLogoutHandler.java
+++ src/main/java/com/github/YewonKimMe/create_spring_app/security/handler/SessionLogoutHandler.java
@@ -2,12 +2,20 @@ package com.github.YewonKimMe.create_spring_app.security.handler;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 
+@Slf4j
 public class SessionLogoutHandler implements LogoutHandler {
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate(); // 세션 완전히 종료
+        }
 
+        log.info("User {} logged out at {}", authentication.getName(), System.currentTimeMillis());
     }
 }
