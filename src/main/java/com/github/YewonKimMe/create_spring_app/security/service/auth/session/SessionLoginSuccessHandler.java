b7ecb72899641bb05ad9dada24f273c63254201d
kim-yewon <kimyewon030428@gmail.com>
2025-11-08 02:19:35 +0900

    SessionLoginSuccessHandler
    - login 로그 추가

--- src/main/java/com/github/YewonKimMe/create_spring_app/security/service/auth/session/SessionLoginSuccessHandler.java
+++ src/main/java/com/github/YewonKimMe/create_spring_app/security/service/auth/session/SessionLoginSuccessHandler.java
@@ -1,13 +1,20 @@
 package com.github.YewonKimMe.create_spring_app.security.service.auth.session;
 
 import com.github.YewonKimMe.create_spring_app.security.service.auth.LoginSuccessHandler;
+import com.github.YewonKimMe.create_spring_app.security.service.auth.Username;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 
+@Slf4j
 public class SessionLoginSuccessHandler implements LoginSuccessHandler {
     @Override
     public void handleLoginSuccess(Authentication auth, HttpServletRequest request, HttpServletResponse response) {
+
+        Username username = new Username(auth.getName());
+
+        log.info("🔓 User '{}' logged in successfully.", username.getName());
 
     }
 }
